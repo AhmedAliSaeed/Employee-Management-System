47f06e64f58a6feeddfa84d76181040a9f0fdf4c
Mostafamahmoud36 <97067785+Mostafamahmoud36@users.noreply.github.com>
2025-03-24 16:28:14 +0200

    Update Employee.cs

--- Employee Management System/Employee.cs
+++ Employee Management System/Employee.cs
@@ -8,5 +8,42 @@ namespace Employee_Management_System
 {
     class Employee
     {
+       public int Id { set ; get ;} 
+       public string Name  { set ; get ;} 
+        public int Age  { set ; get ;}
+        public double Salary  { set ; get ;} 
+        public string Department  { set ; get ;}
+         public DateTime EmploymentDate { get; set;}
+        
+    public Employee( int id, string name, int age, double salary, string department, DateTime employmentDate)
+    {
+        Id = id;
+        Name = name;
+        Age = age;
+        Salary = salary;
+        Department = department;
+        EmploymentDate = employmentDate;
+     }
+
+         public void Promote(double salaryIncrement)
+        {
+            Salary += salaryIncrement;
+            Console.WriteLine($"{Name} has been promoted with a salary increment {salaryIncrement}");
+
+         }  
+        
+         public void Transfer(string newDepartment)
+        {
+            Department = newDepartment;
+            Console.WriteLine($"{Name} has been transferred to {newDepartment} department");
+        }
+        
+       public void Terminate()
+        {
+            Console.WriteLine($"{Name} has been terminated from the company");
+        }
+
+        
+           
     }
 }
