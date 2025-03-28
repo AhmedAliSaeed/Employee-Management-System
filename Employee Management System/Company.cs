c4255eb4be67fbcfa61797ec2708ce087a6b90e5
Omnyaahmed959 <omnyaahmed959@gmail.com>
2025-03-28 19:09:27 +0200

    Update Company.cs

--- Employee Management System/Company.cs
+++ Employee Management System/Company.cs
@@ -8,5 +8,41 @@ namespace Employee_Management_System
 {
     class Company
     {
+        private List<Department> Departments;
+        private List<Employee> Employees;
+
+        public Company()
+        {
+            Departments = new List<Department>();
+            Employees = new List<Employee>();
+        }
+        public void AddDepartment(Department dept)
+        {
+            Departments.Add(dept);
+            Console.WriteLine($" Department {dept.Name} has been added to the company.");
+        }
+        public void AddEmployee(Employee emp, string deptName)
+        {
+            var dept = Departments.Find(d => d.Name == deptName);
+            if (dept != null)
+            {
+                dept.AddEmployee(emp);
+                Employees.Add(emp);
+            }
+            else
+            {
+                Console.WriteLine(" Department not found.");
+            }
+        }
+        public void GenerateReport()
+        {
+            Console.WriteLine("\n Employee Report:");
+            foreach (var dept in Departments)
+            {
+                Console.WriteLine($"\n {dept.Name}");
+                dept.ListEmployees();
+            }
+        }
+
     }
 }
