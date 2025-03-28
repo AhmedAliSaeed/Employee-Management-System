3e38d4182a8bd6800e476320a74af0750ccbd4d2
maiabdallah7 <maiabdalaa2019@gmail.com>
2025-03-29 01:50:48 +0200

    Department attributes and basic functions

--- Employee Management System/Department.cs
+++ Employee Management System/Department.cs
@@ -8,5 +8,38 @@ namespace Employee_Management_System
 {
     class Department
     {
+        private string Name, Head;
+        private List<Employee> Employees;
+
+        Employee employee;
+        public Department(string name, string head)
+        {
+            Name = name;
+            Head = head;
+            Employees = new List<Employee>();
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            if(Employees.Contains(employee))
+                Console.WriteLine($"Employee is already exists in this department");
+            else
+            {
+                Employees.Add(employee);
+                Console.WriteLine("Employee added Successfully");
+            }
+        }
+
+        public void RemoveEmployee()
+        {
+            if (!(Employees.Contains(employee)))
+                Console.WriteLine($"Employee is already doesn't exist in this department");
+            else
+            {
+                Employees.Remove(employee);
+                Console.WriteLine("Employee removed Successfully");
+            }
+            
+        }
     }
 }
