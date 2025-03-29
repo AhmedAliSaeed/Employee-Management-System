32f7b926c142becb19a287f6bf958c411463fd3f
maiabdallah7 <maiabdalaa2019@gmail.com>
2025-03-29 05:11:26 +0200

    Department updates

--- Employee Management System/Department.cs
+++ Employee Management System/Department.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,25 +22,63 @@ namespace Employee_Management_System
 
         public void AddEmployee(Employee employee)
         {
-            if(Employees.Contains(employee))
-                Console.WriteLine($"Employee is already exists in this department");
-            else
-            {
-                Employees.Add(employee);
-                Console.WriteLine("Employee added Successfully");
-            }
+            //if(employee.Department == Name)
+            //{
+                if (Employees.Contains(employee))
+                    Console.WriteLine($"{employee.Name} is already exists in this department\n");
+                else
+                {
+                    Employees.Add(employee);
+                    Console.WriteLine($"{employee.Name} added Successfully\n");
+                }
+            //}
+            /*else
+                Console.WriteLine($"Can not add {employee.Name} to {Name} department because his role doesn't match!");*/
         }
 
-        public void RemoveEmployee()
+        public void RemoveEmployee(Employee employee)
         {
-            if (!(Employees.Contains(employee)))
-                Console.WriteLine($"Employee is already doesn't exist in this department");
+            if (!Employees.Contains(employee))
+                Console.WriteLine($"{employee.Name} is already doesn't exist in this department\n");
             else
             {
                 Employees.Remove(employee);
-                Console.WriteLine("Employee removed Successfully");
+                Console.WriteLine($"{employee.Name} removed Successfully\n");
             }
             
+        }
+
+        public void SwitchDepartment(Employee employee, Department newDepartment)
+        {
+            if (!Employees.Contains(employee))
+            {
+                Console.WriteLine($"Employee {employee.Name} is not in {Name}, so cannot be switched.\n");
+                return;
+            }
+
+            Employees.Remove(employee);
+            Console.WriteLine($"Employee {employee.Name} removed from {Name}.");
+
+            newDepartment.AddEmployee(employee);
+            Console.WriteLine($"Employee {employee.Name} transferred to {newDepartment.Name}.\n");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Department: {Name}");
+            Console.WriteLine($"Department Head: {Head}");
+            Console.WriteLine("Employees:");
+            if (Employees.Count > 0)
+            {
+                foreach (var employee in Employees)
+                {
+                    Console.WriteLine($"   {employee} ");
+                }
+            }
+            else
+                Console.WriteLine("  No employees yet.");
+
+            Console.WriteLine();
         }
     }
 }
--- Employee Management System/Program.cs
+++ Employee Management System/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Department department1 = new Department("Development", "Mai");
+            //Department department2 = new Department("Testing", "Mai");
+            //Department department3 = new Department("Sales", "Alyaa");
+
+            //Employee employee1 = new Employee(1, "Shahenda", 22, 20000, "Development");
+            //Employee employee2 = new Employee(2, "Maha", 22, 20000, "Development");
+            //department1.AddEmployee(employee1);
+            //department2.AddEmployee(employee2);
+            //department1.Display();
+            //department1.SwitchDepartment(employee1, department2);
+            //department1.RemoveEmployee(employee1);
+            //department2.RemoveEmployee(employee1);
+
+
         }
     }
 }
