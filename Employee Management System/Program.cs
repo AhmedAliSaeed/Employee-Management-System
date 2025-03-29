85703acb588b39277728bfc83b4266f0e04ea3e3
Mostafamahmoud36 <97067785+Mostafamahmoud36@users.noreply.github.com>
2025-03-29 03:45:09 +0200

    Update Program.cs

--- Employee Management System/Program.cs
+++ Employee Management System/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Employee emp = new Employee(1,"mostafa",25, 30000,"hr", DateTime.Now);
+            Console.WriteLine(emp.ToString());
+
+            Employee emp1 = new Employee(2,"omer mohamed", 22 , 50000,"it", DateTime.Now);
+            Console.WriteLine(emp1.ToString());
         }
     }
 }
